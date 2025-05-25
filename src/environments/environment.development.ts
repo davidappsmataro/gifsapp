97ea7e2142270d67b8039b8827ad73f18468505c
David <david@grupcm.es>
2025-05-25 06:03:12 +0200

    Eliminados archivos con varibles de entorno con api_keys del repo

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  //giphyApiKey: 'JxqMlOxUXqPJBqeAPL77L0Hc53Ci9bgN',
-  giphyApiKey: 'nCCxMG39NtLjmnnuJNl2ZMIvSVKMidhJ',
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  giphyApiKey: 'nCCxMG39NtLjmnnuJNl2ZMIvSVKMidhJ',
-};
